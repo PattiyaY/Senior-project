c7c65818fe4cf3accd82866eadef7f7bc1344e25
PattiyaY <pattiya.2547@icloud.com>
2024-10-17 23:39:53 +0700

    feat: pictures and code diagram

---
+++ system-architecture.py
@@ -0,0 +1,32 @@
+from diagrams import Cluster, Diagram
+from diagrams.custom import Custom
+from diagrams.programming.framework import React
+from diagrams.onprem.vcs import Gitlab
+from diagrams.onprem.database import MongoDB
+from diagrams.onprem.queue import RabbitMQ
+from diagrams.onprem.iac import Terraform
+from diagrams.azure.compute import AutomanagedVM
+
+
+with Diagram("IDP System Architecture", show=False):
+    user = Custom("User", "./user.png")
+
+    with Cluster("Frontend"):
+        frontend = React("React")
+        authen = Custom("NextAuth.js", "./nextauthjs.png")
+        manageUser = Gitlab("GitLab")
+        frontend - authen
+        frontend - manageUser
+
+    with Cluster("Backend"):
+        backend = MongoDB("MongoDB")
+        queue = RabbitMQ("RabbitMQ")
+        infra = Terraform("Terraform")
+        cloud = AutomanagedVM("Azure VM")
+
+    user >> frontend
+    frontend >> queue
+    queue >> frontend
+    queue >> infra
+    queue >> backend
+    infra >> cloud
